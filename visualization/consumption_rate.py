fixed the map to show average over 4 years

visualization/consumption_rate.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as mp
 
-
-def consumption_rate(df_static, log_scale=True):
-    cons_commune = df_static.groupby(["Consommation annuelle moyenne de la commune (MWh)"]).size()
-    if log_scale:
-        cons_commune = np.log(cons_commune)
-    return cons_commune
+def consumption_rate(df_static):
+    p1 = df_static.plot(x="Année", y=["Consommation annuelle totale de l'adresse (MWh)"], kind="bar", figsize=(9, 8))
+    return p1
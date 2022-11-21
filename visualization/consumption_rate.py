3d6d0737bde248122bc310d14f19c8ed76678970
rimhajal <rim.hajal@gmail.com>
2022-11-21 15:32:26 +0100

    fixed the map to show average over 4 years

--- visualization/consumption_rate.py
+++ visualization/consumption_rate.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as mp
 
-
-def consumption_rate(df_static, log_scale=True):
-    cons_commune = df_static.groupby(["Consommation annuelle moyenne de la commune (MWh)"]).size()
-    if log_scale:
-        cons_commune = np.log(cons_commune)
-    return cons_commune
+def consumption_rate(df_static):
+    p1 = df_static.plot(x="Année", y=["Consommation annuelle totale de l'adresse (MWh)"], kind="bar", figsize=(9, 8))
+    return p1
--- visualization/map_creation.py
+++ visualization/map_creation.py
@@ -1,11 +1,22 @@
-import pygal_maps_fr
-import pygal
+import pandas as pd
+import os
+import geopandas as gpd
+import folium
+import branca.colormap as cm
+from branca.colormap import linear
+import json
+import requests
 
-def map_creation(cons_commune):
-    fr_chart = pygal.maps.fr.Departments(human_readable=True)
-    fr_chart.title = "Yearly Power Consumption Average by Region in France"
+def map_creation(df_static):
+    url = "https://www.data.gouv.fr/fr/datasets/r/fb3580f6-e875-408d-809a-ad22fc418581"
+    #json file with the coordinates of each commune in France
+    geo_json_data = json.loads(requests.get(url).text)
+    commune_avg = df_static.groupby('Nom de la commune', as_index=False)['Consommation annuelle moyenne de la commune (MWh)'].mean()
+    mymap = folium.Map(location=[46.2276,2.2137], zoom_start=5.5)
+    folium.GeoJson(geo_json_data,zoom_on_click=True).add_to(mymap)
+    colormap = linear.YlGn_09.scale(
+        commune_avg['Consommation annuelle moyenne de la commune (MWh)'].min(), commune_avg['Consommation annuelle moyenne de la commune (MWh)'].max()
+    )
 
-    fr_chart.add("Consumption", cons_commune.to_dict())
 
-    fr_chart.render_in_browser()
 
