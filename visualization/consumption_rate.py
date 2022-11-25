42bb00f1216d0c0a9643e731284c7ae960dd5768
rimhajal <rim.hajal@gmail.com>
2022-11-25 10:54:33 +0100

    this isn't needed

--- visualization/consumption_rate.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as mp
-
-def consumption_rate(df_static):
-    p1 = df_static.plot(x="Année", y=["Consommation annuelle totale de l'adresse (MWh)"], kind="bar", figsize=(9, 8))
-    return p1
