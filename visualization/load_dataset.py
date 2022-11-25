68eb4cc3e7c5e1aae6b21e67dae8e874303d3e21
rimhajal <rim.hajal@gmail.com>
2022-11-25 10:54:07 +0100

    fixed coordinates, issue with interactive map

--- visualization/load_dataset.py
+++ visualization/load_dataset.py
@@ -3,13 +3,11 @@ import pandas as pd
 import pooch
 
 url_db = "https://data.enedis.fr/explore/dataset/consommation-annuelle-residentielle-par-adresse/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B"
-path_target = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "..", "data", "power_consumption.csv"
-)
+path_target = "./power_consumption.csv"
 
 class load_dataset:
     def __init__(self, url=url_db, target_name=path_target):
-        path, fname = os.path.split()
+        path, fname = os.path.split(path_target)
         pooch.retrieve(url, path=path, fname=fname, known_hash=None)
 
     @staticmethod
@@ -19,3 +17,4 @@ class load_dataset:
             sep=";",
         )
         return df_static
+
--- visualization/map_creation.py
+++ visualization/map_creation.py
@@ -2,24 +2,22 @@ import pandas as pd
 import os
 import geopandas as gpd
 import folium
-import branca.colormap as cm
-from branca.colormap import linear
-import json
-import requests
+import pyogrio
 
 def map_creation(df_static):
-    url = "https://www.data.gouv.fr/fr/datasets/r/fb3580f6-e875-408d-809a-ad22fc418581"
-    #json file with the coordinates of each commune in France
-    geo_json_data = json.loads(requests.get(url).text)
+    #json file with the coordinates of each commune in France imported as dataframe
+    #capitalized everything because it is case sensitive 
+    geo_json_data['libgeo'] = geo_json_data['libgeo'].str.upper()
+    
     commune_avg = df_static.groupby('Nom de la commune', as_index=False)['Consommation annuelle moyenne de la commune (MWh)'].mean()
+    commune_avg['Nom de la commune'] = commune_avg['Nom de la commune'].str.upper()
+    
+    #creating the dataframe that only contains the communes in both dataframes with their coordinates and consumption rates
+    geo_json_data=geo_json_data.loc[:,('libgeo','geometry')]
+    df_final = geo_json_data.merge(commune_avg, left_on='libgeo', right_on='Nom de la commune',how='outer')
+    df_final.dropna()
 
-
-    for x in geo_json_data["features"]: #filtering out the communes that don't exist in our database
-        if x['properties']['libgeo'].upper()!=commune_avg['Nom de la commune'].str.upper().all(): #capitalized everything because it is case sensitive 
-            geo_json_data["features"].remove(x)
-    geo_json_data['features'].sort(key=lambda e: e['properties']['libgeo'])
-    geo_json_data['features']
-
+    #adding our data to the map
     mymap = folium.Map(location=[46.2276,2.2137], zoom_start=5.5)
     mymap.choropleth(
         geo_data=geo_json_data,
@@ -33,6 +31,32 @@ def map_creation(df_static):
         legend_name='Consommation moyenne des 4 dernières années (MWh)',
         smooth_factor=0
         )
+    
+    #designing our interactive map
+    style_function = lambda x: {'fillColor': '#ffffff', 
+                            'color':'#000000', 
+                            'fillOpacity': 0.1, 
+                            'weight': 0.1}
+    highlight_function = lambda x: {'fillColor': '#000000', 
+                                'color':'#000000', 
+                                'fillOpacity': 0.50, 
+                                'weight': 0.1}
+    
+    #adding our interactive features
+    folium.features.GeoJson(
+        data=df_final,
+        style_function=style_function, 
+        control=False,
+        highlight_function=highlight_function, 
+        tooltip=folium.features.GeoJsonTooltip(
+            fields=['Nom de la commune','Consommation annuelle moyenne de la commune (MWh)'],
+            aliases=['Neighborhood: ','Resident foreign population in %: '],
+            style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"),
+            localize=True,
+            sticky=True,
+            labels=True,
+            )
+        ).add_to(mymap)
     folium.LayerControl().add_to(mymap)
     return(mymap)
     
