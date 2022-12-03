3b28a5544f1cba5ee38e06886782b83edcaec785
rimhajal <rim.hajal@gmail.com>
2022-12-03 10:38:43 +0100

    more init.py changes

--- map_inter.py
+++ map_inter.py
@@ -1,5 +1,5 @@
-import mapstatic
+import mapstatic.visualization as mv
 
-data = mapstatic.load_dataset().save_as_df()
-geo_json_data=mapstatic.load_geojsondata().save_as_df2()
-mymap=mapstatic.map_creation(data,geo_json_data)
+data = mv.dataset.load_dataset().save_as_df()
+geo_json_data=mv.geodata.load_geojsondata().save_as_df2()
+mymap=mv.vis.map_creation(data,geo_json_data)
