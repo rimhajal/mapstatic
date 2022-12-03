8161d05b27c5b513ecd6f574c78b62bc1d5c6268
rimhajal <rim.hajal@gmail.com>
2022-12-03 11:01:38 +0100

    not recognizing module visualization

--- map_inter.py
+++ map_inter.py
@@ -1,5 +1,5 @@
-import mapstatic.visualization as mv
+from mapstatic import visualization 
 
-data = mv.dataset.load_dataset().save_as_df()
-geo_json_data=mv.geodata.load_geojsondata().save_as_df2()
-mymap=mv.vis.map_creation(data,geo_json_data)
+data = visualization.load_dataset().save_as_df()
+geo_json_data=visualization.load_geojsondata().save_as_df2()
+mymap=visualization.map_creation(data,geo_json_data)
