e2a8be502cb15e796239e976dccc889bb71220a6
lilouzulewski <114218057+lilouzulewski@users.noreply.github.com>
2022-12-04 13:19:15 +0100

    update conf.py

--- documentation/conf.py
+++ documentation/conf.py
@@ -2,9 +2,17 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+import os
+import sys
+import warnings
 import sphinx_rtd_theme
 
+sys.path.insert(0, os.path.abspath('..'))
+master_doc = 'index'
+add_module_names = False
+
+def setup(app):
+    app.add_css_file(os.path.join("css","custom.css"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -31,7 +39,20 @@ language = 'English'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-pygments_style = 'vim'
-
-
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+pygments_style = 'monokai'
 html_static_path = ['_static']
