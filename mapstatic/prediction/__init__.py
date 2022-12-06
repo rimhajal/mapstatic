a894be0d2f61c774311cf4f88e77faa16e43b716
maryem-elyamani <114130711+maryem-elyamani@users.noreply.github.com>
2022-12-06 22:07:55 +0100

    Update __init__.py

--- mapstatic/prediction/__init__.py
+++ mapstatic/prediction/__init__.py
@@ -3,3 +3,6 @@ import os
 from datadownloading import *
 from datashaping import *
 from datamodeling import *
+from statsmodels.tools.eval_measures import rmse
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
