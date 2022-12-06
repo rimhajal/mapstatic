211612c92eb84491c799a51f95b246293b7950ff
lilouzulewski <lilou.zulewski@gmail.com>
2022-12-06 22:24:11 +0100

    pred

--- mapstatic/prediction/__init__.py
+++ mapstatic/prediction/__init__.py
@@ -1,8 +1,9 @@
 #importing modules
 import os
-from datadownloading import *
-from datashaping import *
-from datamodeling import *
+from  import *
+from .meanmethod.datadownloading import datadownloading
+from .meanmethod.datashaping import datashaping
+from .meanmethod.datamodeling import prediction
 from statsmodels.tools.eval_measures import rmse
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
