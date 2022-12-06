48302903370d931ce7b43fc56fd97e2984d10788
lilouzulewski <lilou.zulewski@gmail.com>
2022-12-06 22:24:58 +0100

    updating init

--- mapstatic/prediction/__init__.py
+++ mapstatic/prediction/__init__.py
@@ -1,9 +1,5 @@
 #importing modules
 import os
-from  import *
 from .meanmethod.datadownloading import datadownloading
 from .meanmethod.datashaping import datashaping
 from .meanmethod.datamodeling import prediction
-from statsmodels.tools.eval_measures import rmse
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
